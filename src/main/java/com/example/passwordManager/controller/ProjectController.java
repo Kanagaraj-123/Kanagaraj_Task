f2e52296f2bc14fa00ca5ede7fc732b85c8e0fb7
Kanagaraj-123 <rkraji105@gmail.com>
2023-09-04 14:03:03 -0700

    Modified controller class

--- src/main/java/com/example/passwordManager/controller/ProjectController.java
+++ src/main/java/com/example/passwordManager/controller/ProjectController.java
@@ -40,7 +40,7 @@ public class ProjectController {
             files.setFileType(file.getContentType());
            // files.setFileContent(new SerialBlob(file.getBytes())); // Import java.sql.Blob
             files.setData(file.getBytes());
-            projectService.saveFile(files); // Create a fileService to save the file
+            projectService.saveFile(files);
             return new ResponseEntity<>("The File Uploaded Successfully", HttpStatus.OK);
         } catch (Exception e) {
             System.out.println(e);
@@ -48,29 +48,6 @@ public class ProjectController {
         }
     }
 
-    /*@GetMapping("/fetchFile/{fileId}")
-    public ResponseEntity<ByteArrayResource> fetchFile(@PathVariable int fileId) throws SQLException {
-        Optional<Files> optionalFiles = projectService.getFile(fileId);
-        if (optionalFiles.isPresent()) {
-            Files files = new Files();
-            byte[] fileData = files.getData();
-            if (fileData != null) {
-            // ByteArrayResource resource = new ByteArrayResource(files.getFileContent().getBytes(1, (int) files.getFileContent().length()));
-            ByteArrayResource resource = new ByteArrayResource(fileData);
-            return ResponseEntity.ok()
-                    .contentType(MediaType.parseMediaType(files.getFileType(files.)))
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment:filename=\"" + files.getFileName() + "\"")
-                    .body(resource);
-        } else {
-            return (ResponseEntity<ByteArrayResource>) ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-        else {
-            return ResponseEntity.notFound().build();        }
-
-
-    }*/
 
     @GetMapping("/download/{fileId}")
     public ResponseEntity<byte[]> downloadFile(@PathVariable Long fileId) {
